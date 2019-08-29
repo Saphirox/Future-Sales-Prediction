58dadca851be5f95a3da3801162d21f779ec9932
Maksym Suprunenko <suprmaks@gmail.com>
2019-08-29 12:59:41 +0300

    Feature engineering update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# sales-prediction
+# Predict Futures sales
  
+ Project divided into 3 section - 
+ 
+ data_exp - EDA, 
+ 
+ Explanatory data analysis consist of three notebook - sales_train, item_category, items and shop. Each notebook describes each dataset. 
+ 
+ feature_eng - feature engineering . 
+ 
+ Feature engineering creates features based on EDA, also it uses time-series feature and statistics features.
+ 
+ model_train - Training a model and submission
+ 
+ model_train notebooks compraise of 4 models - lgbm, stacking, xgboost, catboost (Unfortunally catboost does not work because took much time to train)
+ 
+ Submission generates model_train notebooks
--- scripts/basic_text_preprocessing.py
+++ scripts/basic_text_preprocessing.py
@@ -14,6 +14,8 @@ from nltk.corpus import stopwords
 import string
 import re
 
+from functools import partial
+
 """
     Class BasicPreprocessText
     
@@ -39,19 +41,32 @@ class BasicPreprocessText:
         strings_data = list(itertools.chain.from_iterable(split_words))
         return strings_data
 
-    def vectorize_process_text(self, text_arr):
-        vectorize_process_text = np.vectorize(self.process_text)
+    def vectorize_process_text(self, text_arr, leave_stop_words=None):
+        if leave_stop_words is None:
+            leave_stop_words = []
+
+        assert isinstance(leave_stop_words, list)
+
+        modified_process_text = partial(self.process_text, leave_stop_words=leave_stop_words)
+        vectorize_process_text = np.vectorize(modified_process_text)
         return vectorize_process_text(text_arr)
 
     def clean_from_stop_words_and_punctuation(self, x):
-        return [word.lower() for word in x.split() if word.lower() not in stopwords.words(self.LANG)]
+        sw = stopwords.words(self.LANG)
+        return [word.lower() for word in x.split() if word.lower() not in sw]
 
-    def process_text(self, text):
+    def process_text(self, text, leave_stop_words):
         # remove html
         text_without_html = BeautifulSoup(text, 'lxml').text
         text_lowercase = text_without_html.lower()
         tokens = self.clean_from_stop_words_and_punctuation(text_lowercase)
-        punctuation_removed = [re.sub("([" + string.punctuation + "]|\s+)+", '', token) for token in tokens]
+
+        punc = list(string.punctuation)
+        for i in leave_stop_words:
+            punc.remove(i)
+        punc = "".join(punc)
+
+        punctuation_removed = [re.sub("([" + punc + "]|\s+)+", '', token) for token in tokens]
         stemmed_words = [self.stemmer.stem(plural) for plural in punctuation_removed]
 
         return " ".join(stemmed_words)
